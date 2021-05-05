57bcf069b7cb21bce3a1da861c237837e63de3ed
Juan Mendoza <juanmendoza0309@gmail.com>
2021-05-04 17:30:58 -0700

    Starting point, working login/register screen

---
+++ src/main/java/com/revature/juan_mendoza_p0/Driver.java
@@ -0,0 +1,40 @@
+package com.revature.juan_mendoza_p0;
+
+import com.revature.juan_mendoza_p0.screens.LoginScreen;
+import com.revature.juan_mendoza_p0.screens.RegisterScreen;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Driver {
+    public static void main(String[] args){
+        System.out.println("Inside");
+
+        System.out.println("Welcome to Sanctuary Banking!");
+        System.out.println("1) Register \n 2) Login");
+
+        int selectionUser = 0;
+        Scanner selScan = new Scanner(System.in);
+        selectionUser = selScan.nextInt();
+
+        if (selectionUser == 1){
+            try(BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in))){
+                RegisterScreen registerScreen = new RegisterScreen((consoleReader));
+                registerScreen.Render();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }else if (selectionUser == 2){
+            try(BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in))){
+                LoginScreen loginScreen = new LoginScreen(consoleReader);
+                loginScreen.Render();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            System.out.println("Please choose again.");
+            selectionUser = selScan.nextInt();
+        }
+    }
+}
