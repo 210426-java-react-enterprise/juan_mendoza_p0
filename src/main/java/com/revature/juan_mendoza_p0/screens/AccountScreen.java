5e31960cf4ad6b7ac3ce5975bd5429c8314b952a
Juan Mendoza <juanmendoza0309@gmail.com>
2021-05-10 23:02:46 -0700

    Account screen complete, need transaction screen and transactionDAO

--- src/main/java/com/revature/juan_mendoza_p0/screens/AccountScreen.java
+++ src/main/java/com/revature/juan_mendoza_p0/screens/AccountScreen.java
@@ -12,9 +12,6 @@ public class AccountScreen extends Screen{
     private BufferedReader consoleReader;
     private ScreenRouter router;
 
-    private double checkingAmount;
-    private double savingAmount;
-
     public AccountScreen(BufferedReader consoleReader,ScreenRouter router) {
 
         super("Account Screen", "/account");
@@ -26,9 +23,29 @@ public class AccountScreen extends Screen{
     @Override
     public void render() {
 
-        System.out.println("Account Screen, choose an option.");
-        System.out.println("1) Deposit");
-        System.out.println("2) Withdraw");
+        System.out.println("Account Options, choose a valid selection:");
+        System.out.println("1) Create Savings Account");
+        System.out.println("2) Create Checking Account");
+        System.out.println("3) Withdraw/Deposit");
+
+        try {
+
+            String userSelection = consoleReader.readLine();
+            switch (userSelection){
+                case "1":
+                    //saving screen
+                    break;
+                case "2":
+                    //checking screen
+                case "3":
+                    //transaction screen
+                default:
+                    System.err.println("Invalid Selection!");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
